6ce8c6c52bc0a2a6289ed2c1b6d7cfdf4b00dd23
Connor Payne <connor@stakehauslabs.com>
2024-07-02 11:13:50 +0200

    better login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:rive/rive.dart';
 import 'package:louderspacemobile/providers/auth_provider.dart';
+import 'registration_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   final TextEditingController _usernameController = TextEditingController();
@@ -13,7 +14,7 @@ class LoginScreen extends StatelessWidget {
       body: Stack(
         children: [
           // Rive animation covering the whole screen
-          Positioned.fill(
+          const Positioned.fill(
             child: RiveAnimation.asset(
               'assets/rive/treesv2.riv',
               fit: BoxFit.cover,
@@ -23,19 +24,32 @@ class LoginScreen extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Spacer(),
+                const Spacer(),
                 TextField(
                   controller: _usernameController,
-                  decoration: InputDecoration(labelText: 'Username'),
-                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
                 ),
+                const SizedBox(height: 10),
                 TextField(
                   controller: _passwordController,
-                  decoration: InputDecoration(labelText: 'Password'),
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
                   obscureText: true,
-                  style: TextStyle(color: Colors.white),
                 ),
                 SizedBox(height: 20),
                 ElevatedButton(
@@ -51,9 +65,39 @@ class LoginScreen extends StatelessWidget {
                       );
                     }
                   },
-                  child: Text('Login'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueAccent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 14),
+                    textStyle: const TextStyle(fontSize: 18),
+                  ),
+                  child: const Center(child: Text('Login',style: TextStyle(color: Colors.white))),
                 ),
-                Spacer(),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Don't have an account?",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => RegistrationScreen()),
+                        );
+                      },
+                      child: const Text(
+                        "Register",
+                        style: TextStyle(color: Colors.blueAccent, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 40), // Additional space at the bottom
               ],
             ),
           ),
